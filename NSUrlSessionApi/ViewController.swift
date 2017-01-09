4b196d172efc6048ec6cde389b918390b063c457
scott-nash01 <shelly.scottnash@technology-research-services.orgshelly.scottnash@technology-research-services.orgshelly.scottnash@technology-research-services.orgshelly.scottnash@technology-research-services.orgshelly.scottnash@technology-research-services>
2017-01-09 10:13:30 -0500

    Initial Commit

---
+++ NSUrlSessionApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSUrlSessionApi
+//
+//  Created by Shelly Scott-Nash on 1/9/17.
+//  Copyright © 2017 Shelly Scott-Nash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
